fb00cf29c5dd76a42632ae466a20627dcd17fec7
Aron <aron.bollmann@web.de>
2020-08-25 16:43:06 +0200

    Edit to_dict()

--- tenantservice/model.py
+++ tenantservice/model.py
@@ -7,7 +7,7 @@ class Tenant(MongoModel):
     last_name = fields.CharField(required=True)
     email = fields.EmailField(required=True)
     gender = fields.CharField(required=True)
-    devices = fields.ListField(field=fields.ObjectIdField(required=False), required=False)
+    devices = fields.ListField(field=fields.ObjectIdField(required=False), default=[])
 
     def clean(self):
         if list(Tenant.objects.raw({'email': self.email})):
@@ -15,5 +15,7 @@ class Tenant(MongoModel):
 
     def to_dict(self):
         as_dict = self.to_son().to_dict()
+        if not self.devices:
+            as_dict['devices'] = []
         del as_dict['_cls']
         return as_dict
