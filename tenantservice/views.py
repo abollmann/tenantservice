02c674efe3b225d7fed4d225c33b0de9b3fee680
Aron <aron.bollmann@web.de>
2020-09-11 11:11:37 +0200

    Add delete path

--- tenantservice/views.py
+++ tenantservice/views.py
@@ -1,23 +1,24 @@
 import json
 
-from flask import request
+from bson import ObjectId
+from flask import request, Response
 from pymodm.errors import ValidationError
 
 from tenantservice import app, logger
 from tenantservice.model import Tenant
 from tenantservice.shared.json_encoder import encode_json
 
-DEVICES_BASE_PATH = '/api/tenants'
+TENANTS_BASE_PATH = '/api/tenants'
 
 
-@app.route(DEVICES_BASE_PATH, methods=['GET'])
+@app.route(TENANTS_BASE_PATH, methods=['GET'])
 def get_all():
     tenants = [tenants.to_dict() for tenants in Tenant.objects.all()]
     logger.warn(F'Found {len(tenants)} entries')
     return encode_json(tenants), 200
 
 
-@app.route(DEVICES_BASE_PATH, methods=['POST'])
+@app.route(TENANTS_BASE_PATH, methods=['POST'])
 def create():
     data = json.loads(request.data.decode('utf-8'))
     try:
@@ -28,3 +29,9 @@ def create():
         logger.error(error.message)
         return error.message, 422
 
+
+@app.route(F'{TENANTS_BASE_PATH}/<tenant_id>', methods=['DELETE'])
+def delete(tenant_id):
+    Tenant.objects.raw({'_id': ObjectId(tenant_id)}).delete()
+    logger.warn(F'Deleted {tenant_id}')
+    return Response(status=204)
